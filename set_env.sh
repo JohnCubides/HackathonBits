943d93e64a6685216893a072edc3509f230fe60d
John Cubides <john.cubides87@gmail.com>
2024-07-30 12:34:29 -0500

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Exportar variables de entorno
-export GITHUB_USERNAME="JohnCubides"
-export GITHUB_REPO="AlgoritmosTipoDatosOperadoresProg"
-export GITHUB_TOKEN="Bearer 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"
+export GITHUB_USERNAME=""
+export GITHUB_REPO=""
+export GITHUB_TOKEN="Bearer "
 export GITHUB_API_URL="https://api.github.com"
